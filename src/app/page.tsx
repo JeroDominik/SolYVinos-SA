4eb5b2859e0f493b4635cb6f9c10bcf28e7b1962
Jero Dominik <jeronimo.dominik@gmail.com>
2024-04-24 13:28:00 -0300

    Add new image and fixing some class in Page and NavBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,21 @@ export default function Home () {
   return (
     <main className="flex flex-col mx-auto items-center justify-center">
       <header className="relative h-full w-full bg-neutral-900">
-        <div className="absolute inset-0 bg-fuchsia-400 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
+        <div className="absolute inset-0 bg-fuchsia-900 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
         <Navbar/>
         <section className="my-10 mx-auto w-[95%]">
-          <h1 className="text-3xl font-semibold text-slate-300 text-center mb-10">
+          <h1 className="text-3xl font-semibold text-slate-300 text-center mb-14">
             Disfrute de Mendoza y la mejor experiencia, con
             <span className="font-bold text-amber-500"> Sol y Vinos</span>
           </h1>
           <article className="flex gap-8 justify-center items-center md:items-start">
-            <Image src="/tunel-cacheuta-potrerillos.webp"
+            <Image src="/tunelPotre2.webp"
               alt="Imagen Tunel Potrerillos"
-              width={500}
-              height={360}
-              className="rounded-xl"
+              width={450}
+              height={400}
+              className="rounded-xl drop-shadow-lg shadow shadow-amber-900 w-2/5"
             />
-            <div>
+            <div className="w-3/5">
               <p className="text-slate-300 text-pretty text-lg mb-2">
                 Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual antes de insertar del texto final.
               </p>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,42 +1,39 @@
 export default function Navbar () {
   return (
-    <>
-      <nav className="mt-4 mb-14 backdrop-blur-xl text-slate-300">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-              <span className="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
-          </a>
-          <div className="items-center justify-between hidden w-full md:flex md:w-auto" id="navbar-language">
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 border-none">
-              <li>
-                <a href="#" className="block py-2 px-3 md:p-0" aria-current="page">Home</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 px-3 md:p-0">About</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 px-3 md:p-0">Services</a>
-              </li>
-              <li>
-                <a href="#" className="block py-2 px-3 md:p-0">Contact</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="flex items-center space-x-1 md:space-x-0">
-              <button type="button" data-dropdown-toggle="language-dropdown-menu" className="inline-flex items-center font-medium justify-center px-4 py-2 text-sm">
-                <svg className="w-5 h-5 rounded-full me-3" aria-hidden="true" viewBox="0 0 3900 3900"><path fill="#b22234" d="M0 0h7410v3900H0z"/><path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" strokeWidth="300"/><path fill="#3c3b6e" d="M0 0h2964v2100H0z"/><g fill="#fff"><g id="d"><g id="c"><g id="e"><g id="b"><path id="a" d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"/><use y="420"/><use y="840"/><use y="1260"/></g><use y="1680"/></g><use x="247" y="210"/></g><use x="494"/></g><use x="988"/><use x="1976"/><use x="2470"/></g></svg>
-                English (US)
-              </button>
-              <button data-collapse-toggle="navbar-language" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden" aria-controls="navbar-language" aria-expanded="false">
-                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
-            </button>
-          </div>
+    <nav className="mt-4 mb-14 backdrop-blur-xl text-slate-300">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+        </a>
+        <div className="md:flex items-center justify-between hidden" id="navbar-language">
+          <ul className="flex flex-col md:flex-row font-medium p-4 md:p-0 mt-4 md:space-x-8 md:mt-0">
+            <li>
+              <a href="#" className="block py-2 px-3 md:p-0" aria-current="page">Inicio</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 md:p-0">Tours</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 md:p-0">Sol y Vinos</a>
+            </li>
+            <li>
+              <a href="#" className="block py-2 px-3 md:p-0">Contact</a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
 
+        <div className="flex items-center">
+            <button type="button" className="inline-flex items-center font-medium justify-center px-4 py-2 text-sm">
+              <svg className="w-5 h-5 rounded-full me-3" aria-hidden="true" viewBox="0 0 3900 3900"><path fill="#b22234" d="M0 0h7410v3900H0z"/><path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" strokeWidth="300"/><path fill="#3c3b6e" d="M0 0h2964v2100H0z"/><g fill="#fff"><g id="d"><g id="c"><g id="e"><g id="b"><path id="a" d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"/><use y="420"/><use y="840"/><use y="1260"/></g><use y="1680"/></g><use x="247" y="210"/></g><use x="494"/></g><use x="988"/><use x="1976"/><use x="2470"/></g></svg>
+                English (US)
+            </button>
+            <button type="button" className="inline-flex items-center p-2 justify-center text-sm rounded-lg md:hidden" aria-controls="navbar-language" aria-expanded="false">
+              <svg className="size-6" aria-hidden="true" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+              </svg>
+          </button>
+        </div>
+      </div>
+    </nav>
   )
 }
