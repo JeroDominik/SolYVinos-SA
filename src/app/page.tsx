f1995492c59bcdb3d8b4ab2f644eb8dce4cdeb12
Jero Dominik <jeronimo.dominik@gmail.com>
2024-04-29 11:49:40 -0300

    Changing some classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home () {
       <header className="relative h-full w-full bg-neutral-900">
         <div className="absolute inset-0 bg-fuchsia-950 bg-[size:20px_20px] opacity-20 blur-[100px]"></div>
         <Navbar/>
-        <section className="my-10 mx-auto w-[95%]">
-          <h1 className="text-4xl font-semibold text-slate-100 text-center mb-14">
-            Disfrute de Mendoza y la mejor experiencia con
-            <span className="font-bold text-amber-500"> Sol y Vinos</span>
+        <section className="mb-14 mt-16 mx-auto max-w-6xl">
+          <h1 className="flex flex-col text-2xl font-semibold text-slate-100 text-center mb-16 gap-y-4">
+            Disfrute de Mendoza y la mejor experiencia
+            <span className="text-5xl font-bold text-amber-600">Sol y Vinos</span>
           </h1>
           <article className="flex gap-8 justify-center items-center md:items-start">
             <Image src="/tunelPotre2.webp"
@@ -22,7 +22,7 @@ export default function Home () {
               className="rounded-xl drop-shadow-lg shadow shadow-amber-900 w-2/5"
             />
             <div className="w-3/5">
-              <p className="text-slate-300 text-pretty text-lg mb-2">
+              <p className="text-slate-300 text-pretty text-lg lg:text-xl mb-2">
                 Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual antes de insertar del texto final.
               </p>
               <p className="text-slate-300 text-pretty text-lg">
@@ -32,12 +32,12 @@ export default function Home () {
           </article>
         </section>
       </header>
-      <section className="mt-20 mb-10 mx-auto w-[95%]">
+      <section className="mt-20 mb-10 mx-auto max-w-6xl">
         <h1 className="flex items-end text-4xl font-bold mb-10 text-slate-900 gap-2">
           <svg className="size-8" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7l6 -3l6 3l6 -3v13l-6 3l-6 -3l-6 3v-13" /><path d="M9 4v13" /><path d="M15 7v13" /></svg>
           Nuestros Tours
         </h1>
-        <div className="grid grid-cols-3 gap-5">
+        <div className="grid grid-cols-3 gap-5 border-slate-300 bg-slate-200 py-10 px-20 rounded-lg">
           <CardTour/>
           <CardTour/>
           <CardTour/>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,12 @@
 export default function Navbar () {
   return (
-    <nav className="mt-4 mb-14 backdrop-blur-xl text-slate-300">
+    <nav className="mb-14 backdrop-blur-xl text-slate-300">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center">
           <span className="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
         </a>
         <div className="md:flex items-center justify-between hidden" id="navbar-language">
-          <ul className="flex flex-col md:flex-row font-medium p-4 md:p-0 mt-4 md:space-x-8 md:mt-0">
+          <ul className="flex flex-col md:flex-row text-xl font-medium p-4 md:p-0 mt-4 md:space-x-8 md:mt-0">
             <li>
               <a href="#" className="block py-2 px-3 md:p-0" aria-current="page">Inicio</a>
             </li>
@@ -23,7 +23,7 @@ export default function Navbar () {
         </div>
 
         <div className="flex items-center">
-            <button type="button" className="inline-flex items-center font-medium justify-center px-4 py-2 text-sm">
+            <button type="button" className="inline-flex items-center font-medium justify-center px-4 py-2 text-xl">
               <svg className="w-5 h-5 rounded-full me-3" aria-hidden="true" viewBox="0 0 3900 3900"><path fill="#b22234" d="M0 0h7410v3900H0z"/><path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" strokeWidth="300"/><path fill="#3c3b6e" d="M0 0h2964v2100H0z"/><g fill="#fff"><g id="d"><g id="c"><g id="e"><g id="b"><path id="a" d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"/><use y="420"/><use y="840"/><use y="1260"/></g><use y="1680"/></g><use x="247" y="210"/></g><use x="494"/></g><use x="988"/><use x="1976"/><use x="2470"/></g></svg>
                 English (US)
             </button>
