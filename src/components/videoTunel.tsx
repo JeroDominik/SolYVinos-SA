4179b1d82ecacceb8a4e4011e752330c8f35df74
Jero Dominik <jeronimo.dominik@gmail.com>
2024-05-24 11:59:46 -0300

    New video format

--- src/components/videoTunel.tsx
+++ src/components/videoTunel.tsx
@@ -2,7 +2,7 @@
 
 export default function VideoTunel () {
   return (
-    <video src={require('../../public/TunelOptimizadoMP4.mp4') }
+    <video src={require('../../public/TunelOptimizadoAppleToo.mp4') }
         className="absolute h-full w-full object-center object-cover top-0 bottom-0 brightness-75"
         autoPlay
         muted
