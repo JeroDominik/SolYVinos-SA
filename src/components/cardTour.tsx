44d74a9708e0225ea52e18a27fc1fec01d4ea4a4
Jero Dominik <jeronimo.dominik@gmail.com>
2024-06-11 14:15:29 -0300

    better quality video, and change Card and Modal Components

--- src/components/cardTour.tsx
+++ src/components/cardTour.tsx
@@ -3,6 +3,8 @@
 import { useState } from 'react'
 import Image from 'next/image'
 
+import CardTourContent from './contentCardTour'
+
 export default function CardTour () {
   const [modalVisible, setModalVisible] = useState(false)
 
@@ -20,33 +22,10 @@ export default function CardTour () {
 
   return (
     <>
-        <div className="relative h-auto w-auto overflow-hidden rounded-xl p-[1px] backdrop-blur-3xl">
-            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#7F44C9_0%,#E2CBFF_50%,#7F44C9_100%)]"/>
-            <div className="flex flex-col h-full w-full items-center justify-center rounded-xl text-sm xl:text-base font-semibold backdrop-blur-3xl bg-stone-900">
-                <Image src="/bodegaFlechadelosAndes.webp"
-                    alt="Imagen Bodega Flecha de los Andes"
-                    width={350}
-                    height={300}
-                    className="rounded-t-xl w-full"
-                />
-                <div className="p-5 w-full">
-                    <h3 className="mb-2 text-xl xl:text-2xl font-bold tracking-tight text-gray-300">
-                        Bodega Flecha de los Andes
-                    </h3>
-                    <p className="mb-3 text-gray-300 line-clamp-5 text-pretty">
-                        Bodega Flechas de los Andes goza de un entorno inigualable y un terroir único donde se dan dos condiciones esenciales para el buen desarrollo de la uva Malbec: clima seco y una gran altitud. Es aquí donde esta variedad tradicional de uva argentina, ha encontrado el lugar ideal para desarrollar todo su potencial.
-                    </p>
-                    <button onClick={showModal} className="inline-flex items-center px-3 py-2 text-center text-base xl:text-lg text-stone-900 rounded-lg bg-gray-300 transition hover:scale-105 hover:text-stone-700">
-                        Vea Más
-                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                    </button>
-                </div>
-            </div>
-        </div>
+        <CardTourContent onButtonClick={showModal} />
 
         {modalVisible && (
-            <div className="fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 bg-black bg-opacity-75 h-full overflow-y-auto container-snap" onClick={handleBackgroundClick}>
+            <div className="fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 bg-black bg-opacity-75 h-full overflow-y-auto" onClick={handleBackgroundClick}>
                 <div className="relative w-[90%] sm:w-[85%] lg:max-w-4xl mx-auto my-6">
                     <div className="flex flex-col items-center relative rounded-lg shadow bg-stone-900">
                         <div className="flex items-center justify-between p-4 md:p-6 border-b rounded-t border-gray-500 gap-x-6 sm:gap-x-10 lg:gap-x-20">
--- src/components/videoTunel.tsx
+++ src/components/videoTunel.tsx
@@ -1,8 +1,6 @@
-'use client'
-
 export default function VideoTunel () {
   return (
-    <video src={require('../../public/TunelOptimizadoAppleToo.mp4') }
+    <video src={require('../../public/TunelOptimizadoAppleHD.mp4')}
         className="absolute h-full w-full object-center object-cover top-0 bottom-0 brightness-75"
         autoPlay
         muted
