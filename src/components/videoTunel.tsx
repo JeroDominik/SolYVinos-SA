a65e0ed238c2d81e8d19cc3e35be21f7c836965d
Jero Dominik <jeronimo.dominik@gmail.com>
2024-06-11 22:01:53 -0300

    video modified

--- src/components/videoTunel.tsx
+++ src/components/videoTunel.tsx
@@ -1,6 +1,6 @@
 export default function VideoTunel () {
   return (
-    <video src={require('../../public/TunelOptimizadoAppleHD.mp4')}
+    <video src={require('../../public/TunelHD.mp4')}
         className="absolute h-full w-full object-center object-cover top-0 bottom-0 brightness-75"
         autoPlay
         muted
