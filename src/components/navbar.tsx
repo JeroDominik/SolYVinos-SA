af4761e5fc143bfe63c204defc9a35c0cf35eb47
Jero Dominik <jeronimo.dominik@gmail.com>
2024-07-14 18:28:28 -0300

    Added Logo.svg to NavBar. And added more Icons

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import Image from 'next/image'
+import { LogoSolyVino, IconUSA } from './Icons/Icons'
 
 export default function Navbar () {
   const [isOpen, setIsOpen] = useState(false)
@@ -12,13 +12,8 @@ export default function Navbar () {
 
   return (
     <section className="fixed top-0 z-40 text-slate-200 w-screen">
-      <section className={`${isOpen ? 'bg-neutral-950' : ''} mx-auto flex items-center justify-between pb-2 w-full custom-nav px-12`}>
-        <Image className="w-28 h-16 lg:w-40 lg:h-28 hover:scale-110 transition rounded-full ml-4"
-          src="/SolyVinosExperience.png"
-          alt="Imagen Sol y Vinos"
-          width={190}
-          height={80}
-        />
+      <section className={`${isOpen ? 'bg-neutral-950' : ''} mx-auto flex items-center justify-between py-2 w-full custom-nav px-12`}>
+        <LogoSolyVino/>
         <div>
           <button onClick={toggleMenu} id="hamburger-button" className={`${isOpen ? 'toggle-btn' : ''} relative size-7 cursor-pointer text-2xl md:hidden mr-4`}>
             <div className="absolute top-4 -mt-0.5 h-1 w-8 rounded bg-slate-300 transition-all duration-500 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-3 before:rounded before:bg-slate-300 before:transition-all before:duration-500 before:content-[''] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-3 after:rounded after:bg-slate-300 after:transition-all after:duration-500 after:content-['']">
@@ -31,7 +26,7 @@ export default function Navbar () {
           </nav>
         </div>
         <button type="button" className="hidden md:inline-flex items-center font-medium justify-center px-4 py-2 text-base sm:text-lg lg:text-xl transition hover:bg-white/10 hover:font-semibold rounded-xl">
-            <svg className="size-5 lg:size-6 rounded-full me-3" aria-hidden="true" viewBox="0 0 3900 3900"><path fill="#b22234" d="M0 0h7410v3900H0z"/><path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" strokeWidth="300"/><path fill="#3c3b6e" d="M0 0h2964v2100H0z"/><g fill="#fff"><g id="d"><g id="c"><g id="e"><g id="b"><path id="a" d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"/><use y="420"/><use y="840"/><use y="1260"/></g><use y="1680"/></g><use x="247" y="210"/></g><use x="494"/></g><use x="988"/><use x="1976"/><use x="2470"/></g></svg>
+            <IconUSA/>
             English
         </button>
       </section>
