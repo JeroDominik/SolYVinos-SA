b5704d2dbe6c62c0d123e15065e5b189ce8d59a3
Jero Dominik <jeronimo.dominik@gmail.com>
2024-08-23 20:09:09 -0300

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/',
-            destination: '/es',
-            permanent: false
-          },
-        ]
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/es',
+        permanent: false
       },
+    ]
+  },
 };
 
 export default nextConfig;
