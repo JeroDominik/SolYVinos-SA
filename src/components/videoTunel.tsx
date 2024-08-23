e20b1c967266334ce743e8a9eb8ca0262faad4b1
Jero Dominik <jeronimo.dominik@gmail.com>
2024-08-23 16:40:37 -0300

    testing

--- src/components/videoTunel.tsx
+++ src/components/videoTunel.tsx
@@ -1,3 +1,5 @@
+const videoSolyVino = process.env.VIDEO_URL
+
 export default function VideoTunel () {
   return (
     <video className="absolute h-full w-full object-center object-cover top-0 bottom-0 brightness-75"
@@ -5,7 +7,7 @@ export default function VideoTunel () {
       muted
       loop
       playsInline>
-      <source src="/videoTesla.webm" type="video/mp4"/>
+      <source src={videoSolyVino} type="video/mp4"/>
     </video>
   )
 }
