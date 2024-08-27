ba6f4966c50363cc3a4b53c8d41b6ce7b118a951
Jero Dominik <jeronimo.dominik@gmail.com>
2024-08-27 16:56:50 -0300

    new propertie in Video tag

--- src/components/videoTunel.tsx
+++ src/components/videoTunel.tsx
@@ -1,12 +1,11 @@
-// const videoSolyVino = process.env.VIDEO_URL
-
 export default function VideoTunel () {
   return (
     <video className="absolute h-full w-full object-center object-cover top-0 bottom-0 brightness-75"
       autoPlay
       muted
       loop
-      playsInline>
+      playsInline
+      preload="auto">
       <source src="/VideoJuanchi1.mp4" type="video/mp4"/>
     </video>
   )
