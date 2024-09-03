c99bb0ebe462680756876cc64a465859d5ffe87a
Jero Dominik <jeronimo.dominik@gmail.com>
2024-09-03 13:07:12 -0300

    New added Benefits

--- src/components/beneficios.tsx
+++ src/components/beneficios.tsx
@@ -1,5 +1,5 @@
 // import Link from 'next/link'
-import { LogoEntreDos, LogoMaguay, LogoMamaJuanchi } from './Icons/Icons'
+import { LogoEntreDos, LogoMaguay, LogoAlquiler, LogoWineShop } from './Icons/Icons'
 
 // interface ExperienceDictionary {
 //   ExperiencesTitle: string
@@ -21,15 +21,18 @@ export default function Beneficios () {
       <h1 className="flex justify-center items-center text-3xl sm:text-4xl lg:text-5xl font-bold text-gray-200 gap-4 my-10">
         Beneficios
       </h1>
-      <ol className="grid grid-cols-2 lg:grid-cols-3 gap-5 px-2 py-10 sm:px-10 xl:px-16 rounded-lg sm:w-[90%] lg:max-w-6xl mx-auto">
+      <ol className="grid grid-cols-2 lg:grid-cols-3 px-2 sm:px-10 xl:px-16 rounded-lg sm:w-[90%] lg:max-w-6xl mx-auto">
         <a className="flex justify-center items-center" href="#">
             <LogoEntreDos/>
         </a>
         <a className="flex justify-center items-center" href="#">
-            <LogoMamaJuanchi/>
+            <LogoAlquiler/>
         </a>
         <a className="flex justify-center items-center" href="#">
             <LogoMaguay/>
+        </a>
+        <a className="flex justify-center items-center" href="#">
+            <LogoWineShop/>
         </a>
       </ol>
     </section>
